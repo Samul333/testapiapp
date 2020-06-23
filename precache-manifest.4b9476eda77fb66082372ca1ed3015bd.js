f67244ca14b38c98215c8536924eb55b19c1f3b9
Samul <samulshrestha101@gmail.com>
2020-06-23 19:47:43 +0545

    Updates

---
+++ precache-manifest.4b9476eda77fb66082372ca1ed3015bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f335be35884f40f5278d771bb392a59",
+    "url": "/testapiapp/index.html"
+  },
+  {
+    "revision": "309b213c661c343411a2",
+    "url": "/testapiapp/static/css/main.432ac949.chunk.css"
+  },
+  {
+    "revision": "9bc57bebb0c4047424e5",
+    "url": "/testapiapp/static/js/2.d75a1e65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testapiapp/static/js/2.d75a1e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309b213c661c343411a2",
+    "url": "/testapiapp/static/js/main.a900606c.chunk.js"
+  },
+  {
+    "revision": "e0af90efea48e7a36634",
+    "url": "/testapiapp/static/js/runtime-main.3250726c.js"
+  },
+  {
+    "revision": "eb78e36f68bd052ef9f76d2d3cd80575",
+    "url": "/testapiapp/static/media/mountain.eb78e36f.jpg"
+  }
+]);
